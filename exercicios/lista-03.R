9779356ec5252127ee1a0b43aa741013c641c7e0
Caio Yamamoto <caioyamamotolellis@usp.br>
2023-08-30 22:51:02 -0300

    .

--- exercicios/lista-03.R
+++ exercicios/lista-03.R
@@ -19,3 +19,36 @@ scatterplot(Recup~Diag, regLine=TRUE, smooth=FALSE, boxplots=FALSE,
 # Coeficientes
 RegModel.3 <- lm(Recup~Diag, data=Dataset_Insetos)
 summary(RegModel.3)
+
+# Criando variável Etario
+Dataset_Insetos <- within(Dataset_Insetos, {
+  Etario <- Recode(Idade, '0:30=0; 30:1000=1', as.factor=TRUE, to.value="=",
+                   interval=":", separator=";")
+})
+
+# Criando variável Cura
+Dataset_Insetos <- within(Dataset_Insetos, {
+  Cura <- Recode(Recup, '0:20="Rápida"; 20:40="Normal"; 40:60="Lenta"', 
+                 as.factor=TRUE, to.value="=", interval=":", separator=";")
+})
+
+# Criando tabela dupla entrada l=Etario c=Coag
+
+library(abind, pos=17)
+local({
+  .Table <- xtabs(~Etario+Coag, data=Dataset_Insetos)
+  cat("\nFrequency table:\n")
+  print(.Table)
+  .Test <- chisq.test(.Table, correct=FALSE)
+  print(.Test)
+})
+
+# Criando uma tabela de dupla entrada com l=Cura e c=Tratamento
+
+local({
+  .Table <- xtabs(~Cura+Tratam, data=Dataset_Insetos)
+  cat("\nFrequency table:\n")
+  print(.Table)
+  .Test <- chisq.test(.Table, correct=FALSE)
+  print(.Test)
+})
